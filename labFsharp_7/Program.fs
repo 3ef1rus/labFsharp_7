2de4b53fc9d2461dc45bd0a285f4bcd3144a6130
3ef1rus <10kmmrza10k@mail.ru>
2022-12-07 23:15:51 +0300

    Добавьте файлы проекта.

---
+++ labFsharp_7/Program.fs
@@ -0,0 +1,34 @@
+﻿let number = "5"
+let answerAgent = 
+    MailboxProcessor.Start(fun inbox->
+        let rec messageLoop() =
+            async{
+            let! msg = inbox.Receive()
+            if msg = number then printfn "%s - ДА" msg
+            else printfn "%s - НЕТ" msg
+            return! messageLoop()
+            }
+        messageLoop()
+     )
+
+let printerAgent = MailboxProcessor.Start(fun inbox->
+ // обработка сообщений
+ let rec messageLoop() = async{
+ // чтение сообщения
+    let! msg = inbox.Receive()
+ // печать сообщения
+    printfn "Сообщение: %s" msg
+    return! messageLoop()
+ }
+ // запуск обработки сообщений
+ messageLoop()
+ )
+
+printerAgent.Post "Сообщение 1"
+printerAgent.Post "Сообщение 2"
+answerAgent.Post "5"
+for i in [1..10] do
+    answerAgent.Post(i.ToString())
+
+    
+    
